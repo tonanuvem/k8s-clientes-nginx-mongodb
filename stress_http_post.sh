a139bc293b8b5775213e1b881d457ec3b1159b64
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2021-11-13 00:30:09 -0300

    Create stress_http_post.sh

---
+++ stress_http_post.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+IP=localhost
+NUM=100
+SLEEP=0.5
+NOW=$(date)
+
+for i in 'seq $NUM'
+do
+  curl -X POST --header 'Content-Type: application/json' --header 'Accept: text/html' -d '{ \ 
+     "fname": "teste_$i_$NOW", \ 
+     "lname": "teste_$i_$NOW" \ 
+   }' 'http://$IP:32500/api/clientes'
+   sleep $SLEEP
+done
