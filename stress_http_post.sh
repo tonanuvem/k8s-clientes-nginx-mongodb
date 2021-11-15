159ed743c822b7b6444019fac6bf9391382424f2
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2021-11-15 09:16:01 -0300

    Update stress_http_post.sh

--- stress_http_post.sh
+++ stress_http_post.sh
@@ -2,7 +2,10 @@
 
 # Hey : script que pode ser usados
 # https://github.com/rakyll/hey
+# curl -s https://hey-release.s3.us-east-2.amazonaws.com/hey_linux_amd64 -o hey && chmod +x hey
 # hey -n 10 -c 2 -m POST -T "application/x-www-form-urlencoded" -d 'username=1&message=hello' http://your-rest-url/resource
+# docker run --rm ricoli/hey -n 10 -c 2 -m POST -T "application/x-www-form-urlencoded" http://localhost:32500/api/clientes -d \
+#   "fname=teste_$RANDOM&lname=teste_$RANDOM"
 
 IP=localhost
 NUM=100
