83f77aab2ee3fbde208eb0e5e680a51a4a930e08
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2021-11-18 14:32:03 -0300

    Update script.js

--- k6_load_teste/script.js
+++ k6_load_teste/script.js
@@ -1,10 +1,8 @@
-//# https://k6.io/docs/testing-guides/api-load-testing/
-
 import http from 'k6/http';
 
 export default function () {
   let msec = "teste_"+Date.parse(Date());
-  const url = 'http://IP/api/clientes';
+  const url = 'http://INSERIR_IP/api/clientes';
   const payload = JSON.stringify({
     fname: msec,
     lname: msec,
