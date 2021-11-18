7d9f644e1c94cfe3b2d6b93712080872bfe0868d
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2021-11-18 14:45:13 -0300

    Update run_k6.sh

--- k6_load_teste/run_k6.sh
+++ k6_load_teste/run_k6.sh
@@ -3,4 +3,4 @@
 
 URL=$(curl -s checkip.amazonaws.com)
 sed -i 's|INSERIR_IP|'$URL'|' script.js
-docker run -i loadimpact/k6 run - <script.js
+docker run -i loadimpact/k6 --vus 1000 --iterations 10 run - <script.js
