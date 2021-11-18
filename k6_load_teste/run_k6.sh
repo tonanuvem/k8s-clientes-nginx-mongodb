5f63cd934104f121b893e5fc0de24b4a6279480f
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2021-11-18 14:57:06 -0300

    Update run_k6.sh

--- k6_load_teste/run_k6.sh
+++ k6_load_teste/run_k6.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # https://k6.io/docs/testing-guides/api-load-testing/
 
-URL=$(curl -s checkip.amazonaws.com)
+echo "Digite o IP para teste de carga"
+read URL
+#URL=$(curl -s checkip.amazonaws.com)
 sed -i 's|INSERIR_IP|'$URL'|' script.js
-docker run -i loadimpact/k6 --vus 1000 --iterations 10000 run - <script.js
+docker run -i loadimpact/k6 --vus 1000 --iterations 10000 run - <$(curl -s https://raw.githubusercontent.com/tonanuvem/k8s-clientes-nginx-mongodb/master/k6_load_teste/script.js)
