2589a2c40142967666b23aa91fadeef1e8fa0977
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2021-11-18 12:42:07 -0300

    Update stress_http_post.sh

--- stress_http_post.sh
+++ stress_http_post.sh
@@ -4,6 +4,7 @@
 # https://github.com/rakyll/hey
 # curl -s https://hey-release.s3.us-east-2.amazonaws.com/hey_linux_amd64 -o hey && chmod +x hey
 # hey -n 10 -c 2 -m POST -T "application/x-www-form-urlencoded" -d 'username=1&message=hello' http://your-rest-url/resource
+# docker run --rm ricoli/hey -n 200 -c 2 -m GET -H "Content-Type: application/json" http://$(curl checkip.amazonaws.com):32500/api/clientes
 # docker run --rm ricoli/hey -n 10 -c 2 -m POST -H "Content-Type: application/json" -d "fname=teste_$RANDOM&lname=teste_$RANDOM" http://$(curl checkip.amazonaws.com):32500/api/clientes 
 
 IP=$(curl checkip.amazonaws.com)
@@ -20,3 +21,9 @@ do
     sleep $SLEEP
     printf "\n"
 done
+
+echo ""
+
+echo "Quantidade de registros inseridos no BD:"
+echo $(curl  -s-X GET "http://$IP:32500/api/clientes" -H "accept: */*" -H "Content-Type: application/json" | grep fname | wc -l)
+echo ""
