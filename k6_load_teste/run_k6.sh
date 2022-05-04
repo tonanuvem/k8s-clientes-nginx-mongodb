f886b69cb55240b8484f0c4a0ce1507b3bd04cbe
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2022-05-04 18:11:16 -0300

    Update run_k6.sh

--- k6_load_teste/run_k6.sh
+++ k6_load_teste/run_k6.sh
@@ -2,7 +2,20 @@
 # https://k6.io/docs/testing-guides/api-load-testing/
 # https://github.com/benc-uk/k6-reporter
 
-echo "Digite o IP para teste de carga"
-read URL
+DIR_ATUAL=$(pwd)
+cd /home/ubuntu/environment/config/shift/
+MASTER=$(terraform output -json ip_externo | jq .[] | jq .[0] | sed 's/"//g')
+URL=$MASTER
+cd $DIR_ATUAL
+
+if [ $(echo $URL | grep . | wc -l) = "0" ]
+then
+  echo "Não foi possível verificar o IP do Master."
+  exit
+fi
+
+echo "IP para teste de carga"
+echo $URL
 sed -i 's|INSERIR_IP|'$URL'|' run_javascript.js
 docker run -i loadimpact/k6 --vus 100 --iterations 2000 run - <run_javascript.js
+sed -i 's|'$URL'|INSERIR_IP|' run_javascript.js
